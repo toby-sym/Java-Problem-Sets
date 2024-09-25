dad1beff2e0ab9afde7ef16af0bee74979aea9a1
Symons <ts01660@heron158.eps.surrey.ac.uk>
2024-09-25 12:25:04 +0100

    Version 1.0

---
+++ ProblemSets/src/main/java/ProblemSet_1a/HelloWorld.java
@@ -0,0 +1,19 @@
+package ProblemSet_1a;
+
+public class HelloWorld
+{
+	private String text;
+
+	public HelloWorld(String helloworld)
+	{
+		text = helloworld;
+	}
+	
+	public String display()
+	{
+		return text;
+	}
+
+
+
+}
