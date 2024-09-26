bcf8006103b42d825e5c04f390e75a283c81d9ed
Symons <ts01660@heron027.eps.surrey.ac.uk>
2024-09-26 14:17:14 +0100

    2b Completed

---
+++ ProblemSets/src/main/java/ProblemSet_2b/Food.java
@@ -0,0 +1,26 @@
+package ProblemSet_2b;
+
+
+public class Food
+{
+	private String foodName;
+	private double foodSugarContent;
+	
+	
+	public Food(String inpfoodName, double inpfoodSugarContent)
+	{
+		foodName = inpfoodName;
+		foodSugarContent = inpfoodSugarContent;
+	}
+	
+	public String getName()
+	{
+		return foodName;
+	}
+	
+	public double getAmountOfSugar()
+	{
+		return foodSugarContent;
+	}
+	
+}
---
+++ ProblemSets/src/main/java/ProblemSet_2b/Meal.java
@@ -0,0 +1,28 @@
+package ProblemSet_2b;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Arrays;
+
+public class Meal
+{
+	private Food food1;
+	private Food food2;
+	
+	public Meal(Food food1, Food food2)
+	{
+		this.food1 = food1;
+		this.food2 = food2;
+	}
+	
+	public double calculateTotalSugarLevel()
+	{
+		return (food1.getAmountOfSugar() + food2.getAmountOfSugar());
+	}
+	
+	public String toString()
+	{
+		return "Meal [mainCourse = " + food1.getName() + ", dessert = " + food2.getName()+"]";
+	}
+	
+}
