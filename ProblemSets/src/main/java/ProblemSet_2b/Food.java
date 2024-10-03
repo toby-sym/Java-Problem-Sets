48d6ed714904313d5e99793e061a6aaa2f85a590
Symons <ts01660@heron036.eps.surrey.ac.uk>
2024-10-03 12:45:54 +0100

    ProblemSet_2b: Style Changes

--- ProblemSets/src/main/java/ProblemSet_2b/Food.java
+++ ProblemSets/src/main/java/ProblemSet_2b/Food.java
@@ -1,26 +1,19 @@
 package ProblemSet_2b;
 
+public class Food {
+    private String foodName;
+    private double foodSugarContent;
 
-public class Food
-{
-	private String foodName;
-	private double foodSugarContent;
-	
-	
-	public Food(String inpfoodName, double inpfoodSugarContent)
-	{
-		foodName = inpfoodName;
-		foodSugarContent = inpfoodSugarContent;
-	}
-	
-	public String getName()
-	{
-		return foodName;
-	}
-	
-	public double getAmountOfSugar()
-	{
-		return foodSugarContent;
-	}
-	
+    public Food(String inpfoodName, double inpfoodSugarContent) {
+        foodName = inpfoodName;
+        foodSugarContent = inpfoodSugarContent;
+    }
+
+    public String getName() {
+        return foodName;
+    }
+
+    public double getAmountOfSugar() {
+        return foodSugarContent;
+    }
 }
