c8354ad3cee358483895f62ed7b023fce010e97f
Symons <ts01660@heron036.eps.surrey.ac.uk>
2024-10-03 12:42:57 +0100

    ProblemSet_2a: Style Changes

--- ProblemSets/src/main/java/ProblemSet_2a/Student.java
+++ ProblemSets/src/main/java/ProblemSet_2a/Student.java
@@ -1,44 +1,33 @@
 package ProblemSet_2a;
 
-public class Student
-{
-	private String firstName;
-	private String lastName;
-	private int age;
-	
-	public Student(String inpfirstName, String inplastName, int inpage)
-	{
-		firstName = inpfirstName;
-		lastName = inplastName;
-		age = inpage;
-	}
-	
-	public void setAge(int newAge)
-	{
-		age = newAge;
-	}
-	
-	public int getAge()
-	{
-		return age;
-	}
-	
-	public String getFullname()
-	{
-		return firstName + " " + lastName;
-	}
-	
-	public void setForename(String newfirstName)
-	{
-		firstName = newfirstName;
-	}
-	
-	public void setSurname(String newlastName)
-	{
-		lastName = newlastName;
-	}
-	
-	
-	
-	
+public class Student {
+    private String firstName;
+    private String lastName;
+    private int age;
+
+    public Student(String inpfirstName, String inplastName, int inpage) {
+        firstName = inpfirstName;
+        lastName = inplastName;
+        age = inpage;
+    }
+
+    public void setAge(int newAge) {
+        age = newAge;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getFullname() {
+        return firstName + " " + lastName;
+    }
+
+    public void setForename(String newfirstName) {
+        firstName = newfirstName;
+    }
+
+    public void setSurname(String newlastName) {
+        lastName = newlastName;
+    }
 }
