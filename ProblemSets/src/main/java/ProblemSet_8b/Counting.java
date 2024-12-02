50cbfaf2035bd8773e2ccf7201228d7697bb296b
Symons <ts01660@heron018.eps.surrey.ac.uk>
2024-12-02 13:12:47 +0000

    ProblemSet_8b: Logic Completed

---
+++ ProblemSets/src/main/java/ProblemSet_8b/Counting.java
@@ -0,0 +1,35 @@
+package ProblemSet_8b;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class Counting
+{
+	private int linesRead = 0;
+	
+	
+	public Counting()
+	{
+	}
+
+	public String readTextFile(String filePath) {
+        StringBuilder result = new StringBuilder();
+        
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            while ((line = reader.readLine()) != null) {
+            	linesRead++;
+                result.append(displayLine(line, linesRead));
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        return result.toString();
+    }
+
+    public String displayLine(String line, int lineNumber) {
+        return line + " : " + lineNumber + "\n";
+    }
+}
