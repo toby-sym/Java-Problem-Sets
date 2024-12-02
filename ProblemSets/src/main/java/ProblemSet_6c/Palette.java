4bb533ee5d06a2e579d250fe6baec10dbefdeb6f
Symons <ts01660@heron018.eps.surrey.ac.uk>
2024-12-02 12:39:08 +0000

    ProblemSet_6c: Logic Completed

--- ProblemSets/src/main/java/ProblemSet_6c/Palette.java
+++ ProblemSets/src/main/java/ProblemSet_6c/Palette.java
@@ -5,33 +5,45 @@ import java.util.Arrays;
 
 public class Palette
 {
-	private ArrayList<P_COLOUR> colours;
-	private String i;
-	private boolean first;
-	
-	public Palette()
-	{
-		colours = new ArrayList<>();
-	}
-	
-	public Palette(P_COLOUR[] colours)
-	{
-		this.colours = new ArrayList<>(Arrays.asList(colours));
-	}
+    private ArrayList<P_COLOUR> colours;
+    private boolean first;
 
-	public void addColour(P_COLOUR colour)
-	{
-		if (colour != null)
-		{
-		colours.add(colour);
-		}
-	}
-	
-	public String getColours() {
+    public Palette()
+    {
+        colours = new ArrayList<>();
+    }
+
+    public Palette(P_COLOUR[] colours)
+    {
+        this.colours = new ArrayList<>(Arrays.asList(colours));
+        if (colours.length > 3)
+        {
+        	throw new IllegalArgumentException("Maximum of 3 Colours permitted in a Palette.");
+        }
+    }
+
+    public void addColour(P_COLOUR colour)
+    {
+        if (colour != null)
+        {
+            if (colours.size() < 3)
+            {
+                colours.add(colour);
+            }
+            else
+            {
+                throw new IllegalStateException("Cannot add more than 3 colours.");
+            }
+        }
+    }
+
+    public String getColours()
+    {
         StringBuilder result = new StringBuilder();
         first = true;
 
-        if (colours.isEmpty()) {
+        if (colours.isEmpty())
+        {
             return "No colours added";
         }
 
@@ -53,5 +65,50 @@ public class Palette
         }
 
         return result.toString();
+    }
+
+    public String mixColours() {
+        colours.removeIf(colour -> colour == null);
+        
+        switch (colours.size()) {
+            case 1:
+                return colours.get(0).name();
+            case 2:
+                P_COLOUR first = colours.get(0);
+                P_COLOUR second = colours.get(1);
+
+                if (first == P_COLOUR.RED) {
+                    if (second == P_COLOUR.YELLOW) {
+                        return "ORANGE";
+                    } else if (second == P_COLOUR.BLUE) {
+                        return "PURPLE";
+                    }
+                } else if (first == P_COLOUR.BLUE) {
+                    if (second == P_COLOUR.YELLOW) {
+                        return "GREEN";
+                    } else if (second == P_COLOUR.RED) {
+                        return "PURPLE";
+                    }
+                } else if (first == P_COLOUR.YELLOW) {
+                    if (second == P_COLOUR.RED) {
+                        return "ORANGE";
+                    } else if (second == P_COLOUR.BLUE) {
+                        return "GREEN";
+                    }
+                }
+                break;
+            case 3:
+                return "BLACK";
+        }
+        return "";
+    }
+
+
+
+    public String display()
+    {
+        StringBuilder displayString = new StringBuilder();
+        displayString.append(getColours()).append(" = ").append(mixColours());
+        return displayString.toString();
     }
 }
