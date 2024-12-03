949db3c582f19a94dfa26a8532165c2e0138a17c
Symons <ts01660@heron196.eps.surrey.ac.uk>
2024-12-03 15:44:55 +0000

    ProblemSet_7a: Stylised

--- ProblemSets/src/main/java/ProblemSet_7a/Module.java
+++ ProblemSets/src/main/java/ProblemSet_7a/Module.java
@@ -1,31 +1,17 @@
 package ProblemSet_7a;
 
-import java.util.regex.*;
+public class Module {
+    private String name;
 
-public class Module
-{
-	private String name;
-	private String nameValidation = "[C,O,M]{3}[0-9]{4}$";
-	
-	
-	
-    public Module(String name)
-    {
-        
-    	this.name = name;
+    public Module(String name) {
+        this.name = name;
     }
-    
-	public String getName()
-	{
-		System.out.println(name.matches(nameValidation));
-		
-		if (!name.matches(nameValidation))
-		{
-			return "Error";
-		}
-		else
-		{
-			return name;
-		}
-	}
+
+    public String getName() {
+        if (!name.matches("[C,O,M]{3}[0-9]{4}$")) {
+            return "Error";
+        } else {
+            return name;
+        }
+    }
 }
