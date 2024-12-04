94eb5b5b416924cbc782d87f53145eb1c0bcfa20
Symons <ts01660@otter01.eps.surrey.ac.uk>
2024-12-04 13:15:51 +0000

    ProblemSet_9b: Stylised

--- ProblemSets/src/main/java/ProblemSet_9b/Charge.java
+++ ProblemSets/src/main/java/ProblemSet_9b/Charge.java
@@ -1,41 +1,36 @@
 package ProblemSet_9b;
 
 public class Charge {
-	private Service service;
-	private double charge;
-	
-	public Charge(Service service, double charge)
-	{
-		if (charge < 0 || charge == 0)
-		{
-			throw new IllegalArgumentException("Invalid Charge");
-		}
-		
-		this.service = service;
-		this.charge = charge;
-	}
-	
-	public Service getService()
-	{
-		return service;
-	}
-	
-	public double getCharge()
-	{
-		return charge;
-	}
-	
-	public double calculateVAT()
-	{
-		switch (service.getRate())
-		{
-			case STANDARD:
-				return (this.charge * 0.2);
-			case LOW:
-				return (this.charge * 0.05);
-			case ZERO:
-				return (this.charge);
-		}
-		return (this.charge);
-	}
+    private Service service;
+    private double charge;
+
+    public Charge(Service service, double charge) {
+        if (charge < 0 || charge == 0) {
+            throw new IllegalArgumentException("Invalid Charge");
+        }
+
+        this.service = service;
+        this.charge = charge;
+    }
+
+    public Service getService() {
+        return service;
+    }
+
+    public double getCharge() {
+        return charge;
+    }
+
+    public double calculateVAT() {
+        switch (service.getRate()) {
+            case STANDARD:
+                return (this.charge * 0.2);
+            case LOW:
+                return (this.charge * 0.05);
+            case ZERO:
+                return this.charge;
+            default:
+                return this.charge; // Adding default case for completeness
+        }
+    }
 }
--- ProblemSets/src/main/java/ProblemSet_9b/Service.java
+++ ProblemSets/src/main/java/ProblemSet_9b/Service.java
@@ -1,39 +1,31 @@
-	package ProblemSet_9b;
-	
-	public class Service {
-		private String code;
-		private String name;
-		private VATRate rate;
-		
-		private final String codeValidation = "^[A-Z]{4}\\d{5}$";
-		
-		public Service(String code, String name, VATRate rate)
-		{			
-			if (name.equals(null) || !(code.matches(codeValidation)))
-			{
-				throw new IllegalArgumentException("Invalid Code Syntax");
-			}
+package ProblemSet_9b;
 
-			this.code = code;
-			this.name = name;
-			this.rate = rate;
-	
-			
-		}
-		
-		public String getCode()
-		{
-			return code;
-		}
-		
-		public String getName()
-		{
-			return name;
-		}
-		
-		public VATRate getRate()
-		{
-			return rate;
-		}
-		
-	}
+public class Service {
+    private String code;
+    private String name;
+    private VATRate rate;
+
+    private final String codeValidation = "^[A-Z]{4}\\d{5}$";
+
+    public Service(String code, String name, VATRate rate) {
+        if (name == null || !code.matches(codeValidation)) { // Fix null check
+            throw new IllegalArgumentException("Invalid Code Syntax");
+        }
+
+        this.code = code;
+        this.name = name;
+        this.rate = rate;
+    }
+
+    public String getCode() {
+        return code;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public VATRate getRate() {
+        return rate;
+    }
+}
