917e32ede3b75b7ece217929bfdca8ab765f1a30
Symons <ts01660@otter01.eps.surrey.ac.uk>
2024-12-04 12:34:04 +0000

    ProblemSet_8a: Stylised

--- ProblemSets/src/main/java/ProblemSet_8a/Customer.java
+++ ProblemSets/src/main/java/ProblemSet_8a/Customer.java
@@ -1,56 +1,40 @@
 package ProblemSet_8a;
 
-public class Customer
-{
-	private String name;
-	private String surname;
-	private final String nameValidation = "^[A-Z][a-z]*$";
-	
-	
-	
-	public Customer(String name, String surname)
-	{
-		if (name.matches(nameValidation) && surname.matches(nameValidation))
-		{
-			this.name = name;
-			this.surname = surname;
-		}
-		else
-		{
-			throw new IllegalArgumentException();
-	    }
-	}
-	
-	public char getName()
-	{
-		return (name.charAt(0));
-	}
-	
-	public void setSurname(String surname)
-	{
-		this.surname = surname;
-	}
+public class Customer {
+    private String name;
+    private String surname;
 
-	public String getSurname() 
-	{
-		return surname;
-	}
-	
-	public boolean validateInfo(String string)
-	{
-		if (string.matches(nameValidation))
-		{
-			return true;
-		}
-		else
-		{
-			return false;
-		}
-	}
-	
-	@Override
-	public String toString()
-	{
-		return (this.getName() + ". " + this.getSurname());
-	}
+    public Customer(String name, String surname) {
+        if (name.matches("^[A-Z][a-z]*$") && surname.matches("^[A-Z][a-z]*$")) {
+            this.name = name;
+            this.surname = surname;
+        } else {
+            throw new IllegalArgumentException();
+        }
+    }
+
+    public char getName() {
+        return name.charAt(0);
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public boolean validateInfo(String string) {
+        if (string.matches("^[A-Z][a-z]*$")) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public String toString() {
+        return this.getName() + ". " + this.getSurname();
+    }
 }
